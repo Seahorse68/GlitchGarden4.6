5290a44577417a1fc3973216125dd2979c8134bf
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-09-28 11:41:06 +0100

    GlitchGarden4.6 Initial Setup

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+public float autoLoadNextLevelAfter;
+
+	void Start() {
+		Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+	}
+
+	public void LoadLevel(string name) {
+		Debug.Log ("Level Load requested for: " + name);
+		Application.LoadLevel (name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Quit Requested");
+	}
+	
+	public void LoadNextLevel() {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+}
