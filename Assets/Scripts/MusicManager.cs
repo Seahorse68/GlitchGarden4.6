6f2bc27f9ed38fc7fd017ed1990ff484b1843dad
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-09-29 16:40:03 +0100

    End of Video 138

--- Assets/Scripts/MusicManager.cs
+++ Assets/Scripts/MusicManager.cs
@@ -25,6 +25,10 @@ public class MusicManager : MonoBehaviour {
 		}
 	}
 	
+	public void ChangeVolume(float volume) {
+		audioSource.volume = volume;
+	}
+	
 	// Update is called once per frame
 	void Update () {
 	
--- Assets/Scripts/PlayerPrefsManager.cs
+++ Assets/Scripts/PlayerPrefsManager.cs
@@ -1,14 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class PlayerPrefsManager : MonoBehaviour {
 
 	const string MASTER_VOLUME_KEY = "master_volume";
 	const string DIFFICULTY_KEY = "difficulty";
 	const string LEVEL_KEY = "level_unlocked_";
 
 	public static void SetMasterVolume(float volume) {
-		if (volume > 0f && volume < 1f) {
+		if (volume >= 0f && volume <= 1f) {
 				PlayerPrefs.SetFloat (MASTER_VOLUME_KEY, volume);
 		} else {
 			Debug.LogError ("Master volume lout of range");
@@ -42,7 +42,7 @@ public class NewBehaviourScript : MonoBehaviour {
 	}
 	
 	public static void SetDifficulty (float difficulty) {
-		if (difficulty >= 0f && difficulty <= 1f) {
+		if (difficulty >= 1f && difficulty <= 3f) {
 			PlayerPrefs.SetFloat (DIFFICULTY_KEY, difficulty);
 		} else {
 			Debug.LogError ("Difficulty out of range");
