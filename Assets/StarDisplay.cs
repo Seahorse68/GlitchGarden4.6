6067e14df30dfd92ed04874759fabc5d1842010e
Seahorse68 <chris@hawes73.freeserve.co.uk>
2016-10-15 12:08:56 +0100

    End of Video 167

---
+++ Assets/StarDisplay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent (typeof(Text))]
+public class StarDisplay : MonoBehaviour {
+
+	private Text starText;
+	private int numberOfStars = 100;
+
+	// Use this for initialization
+	void Start () {
+		starText = GetComponent<Text>();
+		starText.text = numberOfStars.ToString ();
+	}
+
+	private void UpdateDisplay ()
+	{
+		starText.text = numberOfStars.ToString ();
+	}
+	
+	public void AddStars (int amount) {
+		numberOfStars += amount;
+		UpdateDisplay ();
+	}  
+	
+	public void UseStars (int amount) {
+		numberOfStars -= amount;
+		UpdateDisplay ();
+	}
+}
